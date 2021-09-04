f1793f2b2bda2501de2954d9f1ad3bfc0ba0ee8e
Riccardo <riccardo.ali.it@gmail.com>
2021-09-04 14:34:40 +0200

    Added early stopping

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from collections import Counter
 from sklearn.model_selection import train_test_split
 from wavenet_model import get_wavenet
 import tensorflow as tf
-from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
 from tensorflow.keras.models import load_model
 import scipy.io as sio
 
@@ -24,6 +24,6 @@ unique_y = data["unique_y"]
 del data
 
 model = get_wavenet(unique_x, unique_y)
-
+es = EarlyStopping(monitor='val_loss', patience=20)
 mc = ModelCheckpoint('best_model.h5', monitor='val_loss', mode='min', save_best_only=True, verbose=1)
-history = model.fit(x_tr, y_tr, batch_size=128, epochs=50, validation_data=(x_val, y_val), verbose=1, callbacks=[mc])
+history = model.fit(x_tr, y_tr, batch_size=128, epochs=1000, validation_data=(x_val, y_val), verbose=1, callbacks=[mc,es])
